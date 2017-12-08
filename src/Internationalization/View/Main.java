af698fea43279f1958acaf4304055f2547d1c438
Manuel Gil <atencion.ms@outlook.com>
2017-12-08 16:13:51 -0500

    Rename Package

--- src/Internationalization/View/Main.java
+++ src/Internationalization/View/Main.java
@@ -1,5 +1,3 @@
-package Internationalization.View;
-
 //    This file is part of Internationalization Project
 //    Internationalization Free GNU Application
 //    Copyright (C) 2017 Manuel Gil.
@@ -35,7 +33,7 @@ package Internationalization.View;
  * @version $Revision: 1.0.0.0001 $ $Date: 12/01/2017 $
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  */
-
+package Internationalization.View;
 
 import java.awt.BorderLayout;
 import javax.swing.JFrame;
